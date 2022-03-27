50659ddee7b76fe2fa3ad005d3170c7ba0094b48
Shubhamabhimanyu <sg40180@gmail.com>
2022-03-28 00:14:14 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import xyz from './assets/images/img1.jpg';
+
 export default function Home() {
   return (
       <>
     <div className="card bg-dark text-white" style={{position:"relative", textAlign:"center"}}>
-  <img src={xyz} class="card-img" alt="..." style={{height:"500px",width:"100%"}} />
+  <img src="e-Shop/images/img1.jpg" class="card-img" alt="..." style={{height:"500px",width:"100%"}} />
   <div style={{position:"absolute",top:"50%",left:"50%",transform: "translate(-50%, -50%)"}}>
       <h1>Welcome to e-Shop</h1>
       <h3>See Our Collection<Link to={"/products"}><img src="https://img.icons8.com/flat-round/48/000000/arrow--v1.png" alt='' /></Link></h3>
